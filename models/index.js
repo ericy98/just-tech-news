0f5b534b60d7c4e7a95a2221b11e9ad76e2faa3f
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-08-13 14:30:02 -0500

    get, post, put, delete post-routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// link id from user to user_id in post model
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// constraint: post belongs to 1 user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
