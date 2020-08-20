2018fa67853cac96d9822060a1b66f69a2c0bc29
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-08-20 16:47:17 -0500

    finish all tests

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,4 @@
-const {format_date, format_plural} = require('../utils/helpers');
+const {format_date, format_plural, format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-08-20 15:48:12');
@@ -12,4 +12,14 @@ test('format_plural() correctly pluralizes words', () => {
 
     expect(word1).toBe('lion');
     expect(word2).toBe('tigers');
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('http://www.coolstuff.com/abcdefg');
+    const url3 = format_url('http://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 });
